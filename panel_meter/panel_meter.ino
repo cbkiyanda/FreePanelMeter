e62c04d99cec2552640278f7dde02cb034a7c623
Thomas Steam <thomassteam@gmail.com>
2018-08-06 13:42:02 -0400

    New features implemented: --Added ability to change units. Bugfix: --fixed tare function.

--- panel_meter/panel_meter.ino
+++ panel_meter/panel_meter.ino
@@ -31,12 +31,12 @@ int view = 0; // Used by Menu() to display appropriate text to screen
 
 //Values used by Input_Setup()
 float to_add = 0.01;
-float lower_voltage = 0;// Also used by Pressure()
-float upper_voltage = 0;// Also used by Pressure()
+float lower_voltage = 0.5;// Also used by Pressure()
+float upper_voltage = 4.5;// Also used by Pressure()
 int CURSOR = 15; //Also used by Output()
 
 //Values used by Output()
-int Pressure_range = 0;// Also used by Presssure()
+int Pressure_range = 50;// Also used by Presssure()
 int add = 1000;
 
 //Values used by Pressure()
@@ -46,6 +46,7 @@ float pressure = 0;
 float upper_integer = 0;
 float lower_integer = 0;
 float slope = 0;
+int previous_tare = 0;
 
 
 void setup() 
@@ -84,7 +85,73 @@ int debounce(int button, int &previous_button_state) // previous_button_state is
   }
 }
 
+float conversion = 1;          //Will assume everything to be in psi
+int unit_types = 0;
+int previous_unit_types = 0;   //Global so that it doesn't change. I need this property 
+                               //as I need to compare what I had to what I have now         
+                               //selected so that I don't the conversion multiply twice.
+float previous_conversion = 1;
 
+void units()
+{
+menu = 0;
+enter = 0;
+while(enter == 0)
+  {
+  enter = debounce(button4, previous_button4_state);
+  up = debounce(button2, previous_button2_state);
+  down = debounce(button3, previous_button3_state);
+/*deals with the wrapping around of options*/
+  if(unit_types > 2)
+    {
+    unit_types = 0;
+    }
+  if(unit_types < 0)
+    {
+    unit_types = 2;
+    }
+  
+  if(up == 1)
+    {
+    unit_types++;
+    }
+  if(down == 1)
+    {
+    unit_types--;
+    }
+  lcd.setCursor(0,0);
+  switch(unit_types)
+    {
+    case 0:
+    lcd.print("psi");
+      break;
+    case 1:
+    lcd.print("kPa");
+      break;
+    case 2:
+    lcd.print("Pa ");
+      break;
+    }
+  }
+switch(unit_types)
+  {
+  case 0:
+  conversion = 1;
+    break;
+  case 1:
+  conversion = 6.89476;
+    break;
+  case 2:
+  conversion = 6894.76;
+    break;
+  }
+if(unit_types != previous_unit_types)
+  {
+  Pressure_range = (Pressure_range*conversion)/previous_conversion;
+  previous_unit_types = unit_types;
+  previous_conversion = conversion; //Order of this statement in the code is important.
+  }
+}
 
 void Input_Setup()
 {
@@ -223,7 +290,7 @@ void Show(int &choice)
   switch(choice)
     {
     case 0:
-    lcd.print("Version 1.6");
+    lcd.print("Version 1.7");
     while(menu != 1) //For some reason the condition menu = 0 doesn't work. It seems that menu can never truly be 0
       {
         menu = debounce(button1, previous_button1_state);
@@ -237,6 +304,9 @@ void Show(int &choice)
     break;
     case 3:
     Output();
+    break;
+    case 4:
+    units();
     break;
     }
 }
@@ -262,6 +332,9 @@ void View(int option, int &last_option)
     case 3:
     lcd.print("Set up output");
     break;
+    case 4:
+    lcd.print("Set units");
+    break;
   }
 }
 
@@ -269,7 +342,6 @@ void Pressure()
 {
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("The Pressure is:");
   menu = 0;
   tare = 0;
   upper_integer = upper_voltage*204.6;
@@ -281,18 +353,34 @@ void Pressure()
     Data = analogRead(Signal_In);
     pressure = (slope*Data) - (slope*lower_integer);
     tare = debounce(button5, previous_button5_state);
-    if(tare == 1 && lower_integer != Data)
+    if(tare == 1 && previous_tare == 0)
     {
       lower_integer = Data;
-      tare = 0;
+      previous_tare = 1;
     }
-    if(tare == 1 && lower_integer == Data)
+    else if(tare == 1 && previous_tare == 1)
     {
       lower_integer = lower_voltage*204.6;
-      tare = 0;
+      previous_tare = 0;
     }
-    lcd.setCursor(0,1);
+    lcd.setCursor(0,0);
     lcd.print(pressure);
+    lcd.print("  ");
+    switch (unit_types)
+      {
+      case 0:
+      lcd.print("psi    ");
+        break;
+      case 1:
+      lcd.print("kPa    ");
+        break;
+      case 2:
+      lcd.print("Pa    ");
+        break;
+      }
+     lcd.setCursor(0,1);
+     lcd.print(Data);
+     lcd.print(" ");
   }
   lcd.clear();
 }
@@ -316,13 +404,13 @@ void loop()
           view--;
           if(view < 0)
           {
-            view = 3;
+            view = 4;
           }
         }
         if(down == 1)
         {
           view++;
-          if(view > 3)
+          if(view > 4)
           {
             view = 0;
           }
