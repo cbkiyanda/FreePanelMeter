d7c2e386963be67ed119ddb066213b88a3b2675c
Thomas Steam <thomassteam@gmail.com>
2018-08-16 12:12:47 -0400

    Features: --Added capabilities for second pressure sensor --Added better cursor with the built-in library

--- panel_meter/panel_meter.ino
+++ panel_meter/panel_meter.ino
@@ -1,8 +1,8 @@
 //Author: Thomas Savage
 //Date: 13 August 2018
 //Subject: RDE research; Pressure Transducer Display
-//Version 1.8 (Adding cursor to Input Setup)
-//This program reads an analog input and allows the user to display a result in the form of a pressure
+//Version 1.8.5
+//This program reads an analog input and allows the user to display a result in the form of a P1_pressure
 #include <LiquidCrystal.h>
 
 const int rs = 12, enable = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
@@ -31,21 +31,30 @@ int view = 0; // Used by Menu() to display appropriate text to screen
 
 //Values used by Input_Setup()
 float to_add = 0.01;
-float lower_voltage = 0.5;// Also used by Pressure()
-float upper_voltage = 4.5;// Also used by Pressure()
+float P1_lower_voltage = 0.5;// Also used by Pressure()
+float P1_upper_voltage = 4.5;// Also used by Pressure()
+float P2_lower_voltage = 0.5;
+float P2_upper_voltage = 4.5;
 int CURSOR = 15; //Also used by Output()
 
 //Values used by Output()
-int Pressure_range = 50;// Also used by Presssure()
+int P1_Pressure_range = 15;// Also used by Presssure()
+int P2_Pressure_range = 30;
 int add = 1000;
 
 //Values used by Pressure()
-int Signal_In = 1;
-int Data = 0;
-float pressure = 0;
-float upper_integer = 0;
-float lower_integer = 0;
-float slope = 0;
+int P1_Signal_In = 1;
+int P2_Signal_In = 0;
+int P1_Data = 0;
+int P2_Data = 0;
+float P1_pressure = 0;
+float P2_pressure = 0;
+float P1_upper_integer = 0;
+float P2_upper_integer = 0;
+float P1_lower_integer = 0;
+float P2_lower_integer = 0;
+float P1_slope = 0;
+float P2_slope = 0;
 int previous_tare = 0;
 
 
@@ -57,7 +66,8 @@ void setup()
   pinMode(button3, INPUT);
   pinMode(button4, INPUT);
   pinMode(button5, INPUT);
-  pinMode(Signal_In, INPUT);
+  pinMode(P1_Signal_In, INPUT);
+  pinMode(P2_Signal_In, INPUT);
 }
 
 int debounce(int button, int &previous_button_state) // previous_button_state is referenced so that the information isn't lost. Is referenced later by the code
@@ -147,7 +157,8 @@ switch(unit_types)
   }
 if(unit_types != previous_unit_types)
   {
-  Pressure_range = (Pressure_range*conversion)/previous_conversion;
+  P1_Pressure_range = (P1_Pressure_range*conversion)/previous_conversion;
+  P2_Pressure_range = (P2_Pressure_range*conversion)/previous_conversion;
   previous_unit_types = unit_types;
   previous_conversion = conversion; //Order of this statement in the code is important.
   }
@@ -156,20 +167,32 @@ if(unit_types != previous_unit_types)
 void Input_Setup()
 {
   int count = 0; //Deals with blinking of cursor
-  menu = 0;
   enter = 0;
   to_add = 0.01;
   CURSOR = 15;
+  int ROW = 0;
+  int row_change = 0;
+
+  lcd.setCursor(0,0);
+  lcd.print("P1");
   lcd.setCursor(5,0);
-  lcd.print(lower_voltage);
+  lcd.print(P1_lower_voltage);
   lcd.print(" - ");
-  lcd.print(upper_voltage);
+  lcd.print(P1_upper_voltage);
+  lcd.setCursor(0,1);
+  lcd.print("P2");
+  lcd.setCursor(5, 1);
+  lcd.print(P2_lower_voltage);
+  lcd.print(" - ");
+  lcd.print(P2_upper_voltage);
+
   while(enter == 0)
   {
-    enter = debounce(button4, previous_button4_state);
+    enter = debounce(button1, previous_button1_state);
     up = debounce(button2, previous_button2_state);
     down = debounce(button3, previous_button3_state);
-    side = debounce(button5, previous_button5_state);
+    side = debounce(button4, previous_button4_state);
+    row_change = debounce(button5, previous_button5_state);
 
     if(side == 1)
     {
@@ -196,46 +219,99 @@ void Input_Setup()
       CURSOR = 5;
     }
 
-    if(CURSOR <= 8 && CURSOR >= 5)
+    if(row_change == 1)
+    {
+    ROW++;
+    }
+
+    if(ROW > 2)
+    {
+    ROW = 0;
+    }
+
+    if(ROW < 0)
+    {
+    ROW = 1;
+    }
+
+    if(CURSOR <= 8 && CURSOR >= 5 && ROW == 0)
     { 
       if(up == 1)
       {    
-      lower_voltage = lower_voltage + to_add;
+      P1_lower_voltage = P1_lower_voltage + to_add;
       }
       if(down == 1)
       {
-        lower_voltage = lower_voltage - to_add;
+        P1_lower_voltage = P1_lower_voltage - to_add;
       }
-      if(lower_voltage > 5)
+      if(P1_lower_voltage > 5)
       {
-        lower_voltage = 0;
+        P1_lower_voltage = 0;
       }
       
     }
-    if(CURSOR <= 15 && CURSOR >= 12)
+    if(CURSOR <= 15 && CURSOR >= 12 && ROW == 0)
     {
       if(up == 1)
       {
-      upper_voltage = upper_voltage + to_add;
+      P1_upper_voltage = P1_upper_voltage + to_add;
       }
       if(down == 1)
       {
-        upper_voltage = upper_voltage - to_add;
+        P1_upper_voltage = P1_upper_voltage - to_add;
       }
-      if(upper_voltage > 5)
+      if(P1_upper_voltage > 5)
       {
-        upper_voltage = 0;
+        P1_upper_voltage = 0;
       }
     }
+
+    if(CURSOR <= 8 && CURSOR >= 5 && ROW == 1)
+    { 
+      if(up == 1)
+      {    
+      P2_lower_voltage = P2_lower_voltage + to_add;
+      }
+      if(down == 1)
+      {
+        P2_lower_voltage = P2_lower_voltage - to_add;
+      }
+      if(P2_lower_voltage > 5)
+      {
+        P2_lower_voltage = 0;
+      }
+      
+    }
+    if(CURSOR <= 15 && CURSOR >= 12 && ROW == 1)
+    {
+      if(up == 1)
+      {
+      P2_upper_voltage = P2_upper_voltage + to_add;
+      }
+      if(down == 1)
+      {
+        P2_upper_voltage = P2_upper_voltage - to_add;
+      }
+      if(P2_upper_voltage > 5)
+      {
+        P2_upper_voltage = 0;
+      }
+    }
+
     count ++;
     if(up == 1 || down == 1)
     {
       lcd.setCursor(5,0);
-      lcd.print(lower_voltage);
+      lcd.print(P1_lower_voltage);
       lcd.print(" - ");
-      lcd.print(upper_voltage);
+      lcd.print(P1_upper_voltage);
+
+      lcd.setCursor(5, 1);
+      lcd.print(P2_lower_voltage);
+      lcd.print(" - ");
+      lcd.print(P2_upper_voltage);
     }
-    lcd.setCursor(CURSOR, 0);
+    lcd.setCursor(CURSOR, ROW);
     if(count < 500)
     {
       lcd.blink();
@@ -248,61 +324,172 @@ void Input_Setup()
   }
   lcd.clear();
 }
-
+  
 void Output()
 {
   lcd.clear();
   enter = 0;
-  CURSOR = 0;
   add = 1000;
-  while(enter == 0)
-  {
-    enter = debounce(button4, previous_button4_state);
-    up = debounce(button2, previous_button2_state);
-    down = debounce(button3, previous_button3_state);
-    side = debounce(button5, previous_button5_state);
-    if(side == 1)
-    {
-      CURSOR++;
-      add = add/10;
-    }
-    if(CURSOR > 3)
-    {
-      CURSOR = 0;
-      add = 1000;
-    }
-    if(up == 1)
-    {
-      lcd.clear();
-      Pressure_range = Pressure_range + add;
-    }
-    if(down == 1)
-    {
-      lcd.clear();
-      Pressure_range = Pressure_range - add;
-    }
+  int row_change = 0;
+  int ROW = 0;
+  int change_add = 0;
+  int count = 0;
+
     lcd.setCursor(0, 0);
-    lcd.print(Pressure_range);
+    lcd.print("P1:");
+    lcd.print(P1_Pressure_range);
+    lcd.print("+");
+    lcd.print(add);
     lcd.print(" ");
     switch (unit_types)
       {
       case 0:
-      lcd.print("psi    ");
+      lcd.print("psi");
         break;
       case 1:
-      lcd.print("kPa    ");
+      lcd.print("kPa");
         break;
       case 2:
-      lcd.print("Pa    ");
+      lcd.print("Pa");
         break;
       }
-    lcd.print("    ");
+
     lcd.setCursor(0,1);
+    lcd.print("P2:");
+    lcd.print(P2_Pressure_range);
     lcd.print("+");
     lcd.print(add);
-    lcd.print("  ");
+    lcd.print(" ");
+    switch (unit_types)
+      {
+      case 0:
+      lcd.print("psi");
+        break;
+      case 1:
+      lcd.print("kPa");
+        break;
+      case 2:
+      lcd.print("Pa");
+        break;
+      }
+    
+
+  while(enter == 0)
+  {
+    enter = debounce(button1, previous_button1_state);
+    up = debounce(button2, previous_button2_state);
+    down = debounce(button3, previous_button3_state);
+    change_add = debounce(button4, previous_button4_state);
+    row_change = debounce(button5, previous_button5_state);
+    
+    if(row_change == 1)
+    {
+    ROW++;
+    }
+    if(ROW > 1)
+    {
+    ROW = 0;
+    }
+
+    if(ROW == 0)
+    {
+        if(change_add == 1)
+        {
+          add = add/10;
+        }
+        if(add == 0)
+        {
+          add = 1000;
+        }
+        if(up == 1)
+        {
+          P1_Pressure_range = P1_Pressure_range + add;
+        }
+        if(down == 1)
+        {
+          P1_Pressure_range = P1_Pressure_range - add;
+        }
+    }
+
+    if(ROW == 1)
+    {
+        if(change_add == 1)
+        {
+          add = add/10;
+        }
+        if(add == 0)
+        {
+          add = 1000;
+        }
+        if(up == 1)
+        {
+          P2_Pressure_range = P2_Pressure_range + add;
+        }
+        if(down == 1)
+        {
+          P2_Pressure_range = P2_Pressure_range - add;
+        }
+    }
+
+    if(up == 1 || down == 1 || change_add == 1)
+    {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("P1:");
+      lcd.print(P1_Pressure_range);
+      lcd.print("+");
+      lcd.print(add);
+      lcd.print(" ");
+      switch (unit_types)
+        {
+        case 0:
+        lcd.print("psi");
+          break;
+        case 1:
+        lcd.print("kPa");
+          break;
+        case 2:
+        lcd.print("Pa");
+          break;
+        }
+  
+      lcd.setCursor(0,1);
+      lcd.print("P2:");
+      lcd.print(P2_Pressure_range);
+      lcd.print("+");
+      lcd.print(add);
+      lcd.print(" ");
+      switch (unit_types)
+        {
+        case 0:
+        lcd.print("psi");
+          break;
+        case 1:
+        lcd.print("kPa");
+          break;
+        case 2:
+        lcd.print("Pa");
+          break;
+        } 
+    }
+
+    lcd.setCursor(0, ROW);
+    if(count < 500)
+    {
+    lcd.blink();
+    }
+    if(count > 500)
+    {
+    lcd.noBlink();
+    }
+    if(count == 1000)
+    {
+    count = 0;
+    }
+
   }
   lcd.clear();
+  
 }
 
 void Show(int &choice)
@@ -310,7 +497,7 @@ void Show(int &choice)
   switch(choice)
     {
     case 0:
-    lcd.print("Version 1.8");
+    lcd.print("Version 1.8.5");
     while(menu != 1) //For some reason the condition menu = 0 doesn't work. It seems that menu can never truly be 0
       {
         menu = debounce(button1, previous_button1_state);
@@ -364,28 +551,33 @@ void Pressure()
   lcd.setCursor(0,0);
   menu = 0;
   tare = 0;
-  upper_integer = upper_voltage*204.6;
-  lower_integer = lower_voltage*204.6;
-  slope = (Pressure_range)/(upper_integer - lower_integer);
+  P1_upper_integer = P1_upper_voltage*204.6;
+  P1_lower_integer = P1_lower_voltage*204.6;
+  P2_upper_integer = P2_upper_voltage*204.6;
+  P2_lower_integer = P2_lower_voltage*204.6;
+  P1_slope = (P1_Pressure_range)/(P1_upper_integer - P1_lower_integer);
+  P2_slope = (P2_Pressure_range)/(P2_upper_integer - P2_lower_integer);
   while(menu == 0)
   {
     menu = debounce(button1, previous_button1_state);
-    Data = analogRead(Signal_In);
-    pressure = (slope*Data) - (slope*lower_integer);
-    tare = debounce(button5, previous_button5_state);
+    P1_Data = analogRead(P1_Signal_In);
+    delay(100);
+    P2_Data = analogRead(P2_Signal_In);
+    P1_pressure = (P1_slope*P1_Data) - (P1_slope*P1_lower_integer);
+    P2_pressure = (P2_slope*P2_Data) - (P2_slope*P2_lower_integer);
+    /*tare = debounce(button5, previous_button5_state);
     if(tare == 1 && previous_tare == 0)
     {
-      lower_integer = Data;
+      P1_lower_integer = P1_Data;
       previous_tare = 1;
     }
     else if(tare == 1 && previous_tare == 1)
     {
-      lower_integer = lower_voltage*204.6;
+      P1_lower_integer = P1_lower_voltage*204.6;
       previous_tare = 0;
-    }
+    }*/
     lcd.setCursor(0,0);
-    lcd.print(pressure);
-    lcd.print("  ");
+    lcd.print(P1_pressure);
     switch (unit_types)
       {
       case 0:
@@ -398,9 +590,26 @@ void Pressure()
       lcd.print("Pa    ");
         break;
       }
-     lcd.setCursor(0,1);
-     lcd.print(Data);
-     lcd.print(" ");
+     lcd.print(P1_Data);
+     lcd.print("  ");
+
+    lcd.setCursor(0,1);
+    lcd.print(P2_pressure);
+    switch (unit_types)
+      {
+      case 0:
+      lcd.print("psi    ");
+        break;
+      case 1:
+      lcd.print("kPa    ");
+        break;
+      case 2:
+      lcd.print("Pa    ");
+        break;
+      }
+     lcd.print(P2_Data);
+     lcd.print("  ");
+     
      delay(100); //To prevent a flickering screen
   }
   lcd.clear();
@@ -446,3 +655,4 @@ void loop()
       }
     }        
 }
+
